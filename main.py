385c0019c66b1ef7dbe83cafa0a47b43ee6e7e2c
Daredemodaisuki <64051837+Daredemodaisuki@users.noreply.github.com>
2023-08-15 23:35:33 +0800

    Add files via upload
    试着加个白屏？

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import psutil
 import subprocess
 from PIL import ImageGrab
 import configparser
 import time
 
+
+import white_win
 
 # 检测屏幕中间 500x500 区域是否全白
 def is_screen_center_all_white(image):
@@ -27,28 +30,40 @@ def start_genshinimpact(genshinimpact_path):
     return True
 
 
+# 原神是否启动
+def check_genshinimpect() -> bool:
+    pl = psutil.pids()
+    # time.sleep(0.25)
+    for pid in pl:
+        if psutil.Process(pid).name() == "YuanShen.exe":
+            print("原神，已启动！ -> pid =", pid)
+            # 绘制白屏+开始检查窗口是否创建
+            white_win.draw_window()
+            return True
+    return False
+
+
 # 主程序
 def main():
     config = configparser.ConfigParser()
     config.read("config.ini", encoding="utf-8")
 
+    # 原神路径、显示器分辨率
     genshinimpact_path = config.get("Paths", "genshinimpact_path")
-# 下面数值为显示器分辨率
     screen_width = 1920
     screen_height = 1080
-
     while True:
         screenshot = ImageGrab.grab(bbox=(0, 0, screen_width, screen_height))
-
         if is_screen_center_all_white(screenshot):
             if start_genshinimpact(genshinimpact_path):
-                print("原神启动！")
+                print("沃趣，白露原！")
+                check_genshinimpect()
+                print("完成")
             else:
                 print("未能成功运行原神")
             break
         else:
             print("未检测到屏幕中间全白")
-
         time.sleep(0.5)
 
 
---
+++ white_win.py
@@ -0,0 +1,60 @@
+import tkinter
+import win32gui
+import time
+import threading
+
+win = tkinter.Tk()
+label = tkinter.Label(win)
+hwnd_title = dict()
+window_yes = False
+
+
+def get_all_hwnd(hwnd, mouse):
+    if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
+        hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
+    # print({hwnd: win32gui.GetWindowText(hwnd)})
+
+
+# 开始检查窗口是否创建
+def if_Genshin_window():
+    global window_yes
+    while not window_yes:
+        win32gui.EnumWindows(get_all_hwnd, 0)
+        for pida in hwnd_title:
+            if hwnd_title[pida] == "原神":
+                window_yes = True
+                print("原神，完全启动！ -> pid =", pida)
+                time.sleep(3)
+                win.quit()
+
+
+def timer():
+    global window_yes
+    sec = 0
+    while not window_yes:
+        time.sleep(0.1)
+        sec = sec + 1
+        label["text"] = "请稍候\n原神，正在启动!"
+        match int(sec/10) % 4:
+            case 1: label["text"] = label["text"] + "\n"
+            case 2: label["text"] = label["text"] + ".\n"
+            case 3: label["text"] = label["text"] + "..\n"
+            case 0: label["text"] = label["text"] + "...\n"
+        label["text"] = label["text"] + str(sec/10)
+
+
+def draw_window():
+    win.title("Wait for Genshin...")
+    win.geometry("1920x1080")
+    win.overrideredirect(True)
+    win.configure(background='white')
+    label["text"] = "请稍候\n原神，正在启动!\n0.0"
+    label.pack()
+    # 参考 https://zhuanlan.zhihu.com/p/342354790
+    task1 = threading.Thread(target=if_Genshin_window)
+    task1.start()
+    task2 = threading.Thread(target=timer)
+    task2.start()
+    print("画窗口")
+    win.mainloop()
+
